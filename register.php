d37cfda62064109e4fdb7c8fa046f3975febc7ef
Shubham Dubey <30700289+shubhamexpo@users.noreply.github.com>
2020-10-01 11:14:42 +0530

    Update register.php

--- register.php
+++ register.php
@@ -4,8 +4,7 @@ if(isset($_POST['submit'])){
     $email = $_POST['email'];
     $hospital_password = $_POST['hospital_password'];
     $hospital_user_name = $_POST['hospital_user_name'];
-    $hospital_user_name = $_POST['hospital_user_name'];
-    $hospital_name = $_POST['hospital_name'];
+
     $register = "INSERT INTO hospital(email,hospital_user_name,hospital_name,hospital_password) VALUES('$email','$hospital_user_name','$hospital_name','$hospital_password')";
     $query = mysqli_query($connection,$register);
     if($query){
